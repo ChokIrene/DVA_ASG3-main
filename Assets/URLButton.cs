b7d8301bb00f58e0b592142be601ae4d35e7fae8
LeongYenZhen <S10221750@connect.np.edu.sg>
2024-07-21 23:21:07 +0800

    HotSpot
    Did hotspot for piano and tv

--- Assets/URLButton.cs
+++ Assets/URLButton.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class URLButton : MonoBehaviour
 {
-    public void OpenURL()
+    public void Piano()
     {
-        Application.OpenURL("http://fischhaus.com/");
-        Debug.Log("is this working?");
+        Application.OpenURL("https://www.play-by-ear.net/product/casio-cdp-s100-portable-digital-piano/");
     }
 
+    public void PianoLesson()
+    {
+        Application.OpenURL("https://seedmusicsingapore.com/pop-piano/#");
+    }
+
+    public void tv()
+    {
+        Application.OpenURL("https://www.samsung.com/sg/tvs/uhd-4k-tv/au7002-65-inch-uhd-4k-smart-tv-ua65au7002kxxs/");
+    }
+
+    public void VocalLesson()
+    {
+        Application.OpenURL("https://seedmusicsingapore.com/pop-vocals/");
+    }
 }
