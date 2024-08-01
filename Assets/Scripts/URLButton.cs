3be419cfcca0f1318b954f7a181c75fc51f4894e
dhensha <S10240369@connect.np.edu.sg>
2024-08-01 19:47:01 +0800

    PianoRoom
    - Done Hotspot
    -Adding Links

--- Assets/Scripts/URLButton.cs
+++ Assets/Scripts/URLButton.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class URLButton : MonoBehaviour
 {
-    public void Piano()
+    public void Beyonce()
     {
-        Application.OpenURL("https://www.play-by-ear.net/product/casio-cdp-s100-portable-digital-piano/");
+        Application.OpenURL("https://en.wikipedia.org/wiki/I_Am..._Sasha_Fierce");
     }
+
+    public void BeyonceAlbum()
+    {
+        Application.OpenURL("https://open.spotify.com/album/3ROfBX6lJLnCmaw1NrP5K9?autoplay=true");
+    }
+
+    public void RayCharles()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/Genius_%2B_Soul_%3D_Jazz");
+    }
+
+    public void RayCharlesAlbum()
+    {
+        Application.OpenURL("https://open.spotify.com/album/1VTSotSPiRvCbAtlQvSS5q?autoplay=true");
+    }
+
+
+    public void ArethaFranklin()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/With_Everything_I_Feel_in_Me");
+    }
+
+    public void ArethaFranklinAlbum()
+    {
+        Application.OpenURL("https://open.spotify.com/track/1fZ2whbTzYukfXCARFYU9y?autoplay=true");
+    }
+
 
     public void PianoLesson()
     {
